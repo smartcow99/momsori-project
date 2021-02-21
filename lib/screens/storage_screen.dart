2fc91d9074c63f100a2a74baef6e289f93afd0c0
smartcow99 <78792358+smartcow99@users.noreply.github.com>
2021-02-21 14:13:36 +0900

    Update storage_screen.dart
    개요만 잡았습니다.

--- lib/screens/storage_screen.dart
+++ lib/screens/storage_screen.dart
@@ -7,15 +7,112 @@ class StorageScreen extends StatefulWidget {
 }
 
 class _StorageScreenState extends State<StorageScreen> {
+  var folders = [];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'StorageScreen',
-        style: TextStyle(
-          fontSize: 32.0,
-          fontWeight: FontWeight.bold,
-        ),
+    final Size size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            child: Row(
+              children: [
+                RaisedButton(
+                  onPressed: () {
+                    folders.add(DateTime.now().toIso8601String());
+                    setState(() {});
+                  },
+                  child: Text(
+                    'new folder',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'edit',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: Text(
+              'Storage',
+              style: TextStyle(
+                fontSize: 64,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            child: Text(
+              'Total Storage',
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return ListTile(title: Text(folders[index]));
+                },
+                itemCount: folders.length,
+              ),
+            ),
+          ),
+          // Container(
+          //   child: RaisedButton(
+          //     onPressed: () {},
+          //     child: Text(
+          //       'Total Folder',
+          //       style: TextStyle(
+          //         fontSize: 32,
+          //         fontWeight: FontWeight.bold,
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          // Container(
+          //   child: RaisedButton(
+          //     onPressed: () {},
+          //     child: Text(
+          //       'Folder 1',
+          //       style: TextStyle(
+          //         fontSize: 32,
+          //         fontWeight: FontWeight.bold,
+          //       ),
+          //     ),
+          //   ),
+          // ),
+          // Container(
+          //   child: RaisedButton(
+          //     onPressed: () {},
+          //     child: Text(
+          //       'Folder 2',
+          //       style: TextStyle(
+          //         fontSize: 32,
+          //         fontWeight: FontWeight.bold,
+          //       ),
+          //     ),
+          //   ),
+          // ),
+        ],
       ),
     );
   }
